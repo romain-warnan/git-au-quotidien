b96f7d21b80e78d315a7da76bb80163c014ece42
Romain Warnan <romain.warnan@insee.fr>
2016-11-07 17:54:33 +0100

    Retour à la correction du TP5

--- src/main/java/fr/insee/bar/controller/ModificationClientController.java
+++ src/main/java/fr/insee/bar/controller/ModificationClientController.java
@@ -1,10 +1,12 @@
 package fr.insee.bar.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import fr.insee.bar.dao.ClientDao;
 import fr.insee.bar.model.Client;
+import fr.insee.bar.validator.ClientValidator;
 
 @Controller
 @RequestMapping("/client")
@@ -21,6 +24,9 @@ public class ModificationClientController extends WebMvcConfigurerAdapter {
 	@Autowired
 	private ClientDao clientDao;
 
+	@Autowired
+	private ClientValidator clientValidator;
+
 	@GetMapping("/modification/{client}")
 	public String modificationClient(@PathVariable("client") Client client, Model model) {
 		model.addAttribute("client", client);
@@ -28,7 +34,12 @@ public class ModificationClientController extends WebMvcConfigurerAdapter {
 	}
 
 	@PostMapping("/modification/{client}")
-	public String modificationClientPost(@ModelAttribute Client client, RedirectAttributes attributes) {
+	public String modificationClientPost(@Valid Client client, BindingResult result, Model model, RedirectAttributes attributes) {
+		clientValidator.validate(client, result);
+		if (result.hasErrors()) {
+			model.addAttribute("client", client);
+			return "modification-client";
+		}
 		clientDao.update(client);
 		attributes.addFlashAttribute("modification", true);
 		return "redirect:/client/" + client.getId();
